5a729fcdca5db05853a31fce657f2a80f6a87b42
dmitriy.savran <dmitriy.savran@faceit.com.ua>
2014-08-07 13:50:56 +0300

    fix

--- lib/spree_checkout_ru/engine.rb
+++ lib/spree_checkout_ru/engine.rb
@@ -3,5 +3,12 @@ module SpreeCheckoutRu
     require 'spree/core'
     isolate_namespace Spree
     engine_name 'spree_checkout_ru'
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare &method(:activate).to_proc
   end
 end
